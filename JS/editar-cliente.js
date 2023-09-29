b4f023dddebaac4c58d3eb5737f5f2f044cad258
Miguel Ruiz <later481@gmail.com>
2023-09-28 23:16:33 -0600

    funcion edicion finalizada

--- JS/editar-cliente.js
+++ JS/editar-cliente.js
@@ -1,5 +1,6 @@
 (function() {
     let DB;
+    let idCliente;
 
     const nombreInput = document.querySelector("#nombre");
     const emailInput = document.querySelector("#email");
@@ -17,7 +18,7 @@
 
         //Verificar el id de la URL
         const parametrosURL = new URLSearchParams(window.location.search);
-        const idCliente = parametrosURL.get("id");
+        idCliente = parametrosURL.get("id");
         // console.log(idCliente);
         if(idCliente){
             setTimeout(() => {
@@ -67,6 +68,26 @@
         if(nombreInput.value === "" || emailInput.value === "" || telefonoInput.value === "" || empresaInput.value === ""){
             imprimirAlerta("Todos los campos son obligatorios", "error");
             return;
+        }
+        //Actualizar cliente
+        const clienteActualizado = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: Number(idCliente)
+        }
+        const transaction = DB.transaction(["crm"], "readwrite");
+        const objectStore = transaction.objectStore("crm");
+        objectStore.put(clienteActualizado);
+        transaction.oncomplete = function(){
+            imprimirAlerta("Editado Correntamente");
+            setTimeout(() => {
+                window.location.href = "index.html";
+            }, 1000)
+        }
+        transaction.onerror = function(){
+            imprimirAlerta("Hubo un error", "error");
         }
     }
 
